31f787d439d975e17db0bfdcb9c4d1c623ba5cd3
codenoid <14269809+codenoid@users.noreply.github.com>
2024-07-29 20:51:18 +0700

    update deps

--- LokalCS/Lokal.cs
+++ LokalCS/Lokal.cs
@@ -3,8 +3,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using Newtonsoft.Json;
-using RestSharp;
-using RestSharp.Authenticators;
 using SemanticVersioning;
 
 namespace LokalCS
